8c349109087bb0d50e9553ad34a55e0e26426793
Alexander Stezhko <stezhko98@gmail.com>
2019-08-01 15:42:31 +0300

    Task commit

---
+++ TaskL3.java
@@ -0,0 +1,14 @@
+public class TaskL3 {
+    public static void main (String[] args){
+        int[] array= {2,6,5,7,9,5,6,7};
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+        for (int i=array.length-1; i <0 ;i--){
+            System.out.println(array[i]);
+        }
+
+
+
+    }
+}
